9d14963002fd57d0ee9c876b6b1204dc220435f5
Your Name <you@example.com>
2019-08-27 22:16:13 -0300

    pagina

--- Clase02/Controles/scripts/funciones.js
+++ Clase02/Controles/scripts/funciones.js
@@ -1,19 +1,27 @@
-/* function SaludarFromJS() {
-    console.log("Hola a todos");
-} */
-
 var btnSaludar;
 /* document es una referencia al html */
 
 /* Manejador de eventos */
+/* cuando se carga la pagina, se dispara el evento load y puedo instanciar el boton*/
+/********** addEventListener ***********/
+/* 1er Parametro: el evento del control */
+/* 2do Parametro: funcion que se ejecuta */
 window.addEventListener('load', function(){
     btnSaludar = document.getElementById('btnSaludarJS');
 
-    btnSaludar.addEventListener('click', function(){
+    /* Opcion 1 */
+     btnSaludar.addEventListener('click', function(){
         console.log('Hola a todos');
         alert('Hola mundo');
-    })
+    }) 
+
+    /* Opcion 2 - Descomentar la funcion */
+    /* btnSaludar.addEventListener('click', SaludarFromJS); */
 })
+
+/*  function SaludarFromJS() {
+    console.log("Hola a todos");
+}  */
 
 /* Flecha gorda: es lo mismo pero sin declarar la function */
 /* window.addEventListener('load', ()=>{
